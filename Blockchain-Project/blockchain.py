699a84521a5cd247d350621460fccb0f126756dd
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-03-22 16:11:14 -0300

    implementação dos blocos de mineração e adicionado o nível de dificuldade hash

--- Blockchain-Project/blockchain.py
+++ Blockchain-Project/blockchain.py
@@ -38,12 +38,37 @@ class Blockchain():
         self.chain = chain
 
     def add(self, block):
-        self.chain.append({'hash': block.hash(), 'previous': block.previous_hash, 'number': block.number, 'data': block.data, 'nonce': block.nonce})
+        self.chain.append({'hash': block.hash(),
+                           'previous': block.previous_hash,
+                           'number': block.number,
+                           'data': block.data,
+                           'nonce': block.nonce
+                           })
 
+    def mine(self, block):
+        try:
+            block.previous_hash = self.chain[-1].get('hash')
+        except IndexError:
+            pass
+
+        while True:
+            if block.hash()[:self.difficulty] == "0" * self.difficulty:
+                self.add(block);
+                break
+            else:
+                block.nonce += 1
 
 def main():
-    block = Block("hello world",1)
-    print(block)
+    blockchain = Blockchain()
+    database = ["hello world", "what's up", "hello", "bye"]
+
+    num = 0
+    for data in database:
+        num += 1
+        blockchain.mine(Block(data,num))
+
+    for block in blockchain.chain:
+        print(block)
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Trabalho-de-conclusao-de-curso
+<!-- Visualizador online: https://stackedit.io/ -->
+ ![Logo da UDESC Alto Vale](http://www1.udesc.br/imagens/id_submenu/2019/marca_alto_vale_horizontal_assinatura_rgb_01.jpg)
+
+
+#Trabalho de conclusão de curso
