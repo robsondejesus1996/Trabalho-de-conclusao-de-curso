0862a76758170a964b95090fb9c6529e91496577
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-03-23 10:36:00 -0300

    Implementação da verificação dos blocos válidos

--- Blockchain-Project/blockchain.py
+++ Blockchain-Project/blockchain.py
@@ -1,17 +1,16 @@
-
-#conjunto de funções hast criptográficas
+# conjunto de funções hast criptográficas
 
 from hashlib import sha256
 
 
 def updatehash(*args):
-    hashing_text = ""; h = sha256()
+    hashing_text = "";
+    h = sha256()
     for arg in args:
         hashing_text += str(arg)
 
     h.update(hashing_text.encode('utf-8'))
     return h.hexdigest()
-
 
 
 class Block():
@@ -20,34 +19,32 @@ class Block():
     nonce = 0
     previous_hash = "0" * 64
 
-    def __init__(self, data,number=0):
+    def __init__(self, data, number=0):
         self.data = data
         self.number = number
 
     def hash(self):
         return updatehash(self.previous_hash, self.number, self.data, self.nonce)
 
-
     def __str__(self):
-        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" %(self.number,self.hash(),self.previous_hash,self.data,self.nonce))
+        return str("Block#: %s\nHash: %s\nPrevious: %s\nData: %s\nNonce: %s\n" % (
+        self.number, self.hash(), self.previous_hash, self.data, self.nonce))
+
 
 class Blockchain():
     difficulty = 4
 
-    def __init__(self,chain=[]):
+    def __init__(self, chain=[]):
         self.chain = chain
 
     def add(self, block):
-        self.chain.append({'hash': block.hash(),
-                           'previous': block.previous_hash,
-                           'number': block.number,
-                           'data': block.data,
-                           'nonce': block.nonce
-                           })
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
 
     def mine(self, block):
-        try:
-            block.previous_hash = self.chain[-1].get('hash')
+        try: block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
@@ -58,6 +55,16 @@ class Blockchain():
             else:
                 block.nonce += 1
 
+    def isValid(self):
+        for i in range(1,len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0"*self.difficulty:
+                return False
+
+        return True
+
+
 def main():
     blockchain = Blockchain()
     database = ["hello world", "what's up", "hello", "bye"]
@@ -65,10 +72,15 @@ def main():
     num = 0
     for data in database:
         num += 1
-        blockchain.mine(Block(data,num))
+        blockchain.mine(Block(data, num))
 
     for block in blockchain.chain:
         print(block)
+
+    blockchain.chain[2].data = "NEW DATA"
+    blockchain.mine(blockchain.chain[2])
+    print(blockchain.isValid())
+
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 <!-- Visualizador online: https://stackedit.io/ -->
  ![Logo da UDESC Alto Vale](http://www1.udesc.br/imagens/id_submenu/2019/marca_alto_vale_horizontal_assinatura_rgb_01.jpg)
 
-
-#Trabalho de conclusão de curso
+# Trabalho de conclusão de curso
