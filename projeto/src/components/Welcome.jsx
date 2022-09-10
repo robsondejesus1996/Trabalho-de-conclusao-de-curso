5fee7436f40bea18e7af15ea0dfa217583486d06
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-10 10:49:51 -0300

    Ajustes grid de informações e cartão eth

--- projeto/src/components/Welcome.jsx
+++ projeto/src/components/Welcome.jsx
@@ -1,7 +1,11 @@
 import { AiFillAlipayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+
 import { Loader } from "./Loader";
+
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justity-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Welcome = () => {
   const connectWallet = () => {};
@@ -26,6 +30,35 @@ const Welcome = () => {
               Conectar Carteira
             </p>
           </button>
+          <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
+                        <div className={`rounded-tl-2xl ${commonStyles}`}>
+                            Seguro </div>
+                            <div className={commonStyles}>
+                            Agilidade
+                            
+                            </div>
+
+                            <div className={`rounded-tr-2xl ${commonStyles}`}>
+                            Ethereum
+                            
+                            </div>
+
+                            <div className={`rounded-bl-2xl ${commonStyles}`}>
+                            Web 3.0 </div>
+
+                            <div className={commonStyles}>
+                            Taxas Atrativas
+                            
+                            </div>
+
+                            <div className={`rounded-br-2xl ${commonStyles}`}>
+                            Metamask </div>
+                    </div>
+        </div>
+        <div className="flex flex-col flex-1 items-center justity-start w-full mf:mt-0 mt-10">
+            <div className="p-3 flex justity-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+
+            </div>
         </div>
       </div>
     </div>
