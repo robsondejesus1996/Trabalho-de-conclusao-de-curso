d887d01d0d4720102306a9eff59638c9f3ea1ac7
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-10 11:21:19 -0300

    Implementação campos de transferência

--- projeto/src/components/Welcome.jsx
+++ projeto/src/components/Welcome.jsx
@@ -7,6 +7,20 @@ import { Loader } from "./Loader";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justity-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+  const Input = ({placeholder, name, type, value, handleChange }) =>(
+    <input
+
+        placeholder={placeholder}
+        type = {type}
+        value = {value}
+        step = "0.0001"
+        onChange = {(e) => handleChange(e, name)}
+        className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+    
+    />
+
+);
+
 const Welcome = () => {
   const connectWallet = () => {};
   return (
@@ -55,11 +69,37 @@ const Welcome = () => {
                             Metamask </div>
                     </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justity-start w-full mf:mt-0 mt-10">
-            <div className="p-3 flex justity-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    
+                                    <SiEthereum font-size={21} color='#fff'/>
+                                </div>
 
-            </div>
-        </div>
+                                    <BsInfoCircle font-size={17} color='#fff'/>
+
+                            </div>
+                            <p className='text-white font-light text-sm'>
+                                Endereço
+                            </p>
+                            <p className="text-white font-semibold text-lg mt-1">
+                                Ethereum
+                            </p>
+
+                        </div>
+                      
+                    </div>
+
+                </div>
+
+                <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
+                    <Input placeholder="Address To" name="addressTo" type="text" handleChange={()=>{}}/>
+                    <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={()=>{}}/>
+                    <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={()=>{}}/>
+                    <Input placeholder="Enter Message" name="message" type="text" handleChange={()=>{}}/>
+                </div>
       </div>
     </div>
   );
