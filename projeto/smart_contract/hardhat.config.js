46fdc870e10532661f1294aa58c34e3dfd72e226
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-11 10:01:00 -0300

    Implementação do deploy endereco do contrato

--- projeto/smart_contract/hardhat.config.js
+++ projeto/smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/97LbN7GZgI_cMw-Hs2LdA-FaYsa-Odzd', 
+      accounts: ['8178e5ec4cddee34a69f48befec1674e8625b3adfe1e31cd8cabec0feeb51de0']
+    }
+  }
 };
---
+++ projeto/src/utils/constantes.js
@@ -0,0 +1,9 @@
+
+import abi from './Transactions.json';
+
+export const contractAbi = abi.abi;
+export const contractAdress = "0xb9966B5634D1d34b108392Aee727D2f2a2fD9e06"
+
+
+
+
