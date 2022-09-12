084e3ee1ce33fbe2d0cfabd5f8a73e23b94ce32e
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 13:29:58 -0300

    Inicialização construção componentes cards

--- projeto/src/components/Services.jsx
+++ projeto/src/components/Services.jsx
@@ -2,6 +2,12 @@ import { BsShieldFillCheck } from "react-icons/bs";
 import { BiSearchAlt } from 'react-icons/bi';
 import { RiHeart2Fill } from 'react-icons/ri';
 
+const ServiceCard = ({color, title, icons, subtitle}) =>(
+    <div className="flex flex-row md:flex-row justify-center items-center ehite-glassmorphism p-3 m-2 cursor-pointer gover:shadow-xl">
+
+    </div>
+)
+
 const Services = () =>{
     return (
         <div className="flex w-full justify-center gradient-bg-services">
@@ -13,6 +19,16 @@ const Services = () =>{
                     </h1>
 
                 </div>
+
+            </div>
+            <div className="flex-1 flex flex-col justity-start items-center">
+                <ServiceCard
+                    color="bg-pink-900"
+                    title="Segurança Garantida"
+                    icons={<BsShieldFillCheck fontSize={21} className="text-white" />}
+                    subtitle="Segurança em Primeiro Lugar. Mantendo a Qualidade e Privacidade"
+                
+                />
 
             </div>
         </div>
