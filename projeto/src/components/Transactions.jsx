3094acd7c2b5b224d52d00062e48bbac73d6a622
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 16:05:18 -0300

    teste de históricos de transações

--- projeto/src/components/Transactions.jsx
+++ projeto/src/components/Transactions.jsx
@@ -1,44 +1,84 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import { TransactionContext } from '../context/TransactionContext';
+import { TransactionContext } from "../context/TransactionContext";
 
-import dummyData from '../utils/dummyData';
-import { shortenAddress } from '../utils/shortenAddress';
+import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
 
+const TransactionCard = ({
+  addressTo,
+  addressFrom,
+  timestamp,
+  message,
+  keyword,
+  amount,
+  url,
+}) => {
+  return (
+    <div className="bg-pink-900 m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
+      <div className="flex flex-col items-center w-full mt-3">
+        <div className="w-full mb-6 p-2">
+          <a
+            href={`https://goerli.etherscan.io/address/${addressFrom}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base ">
+              {" "}
+              De: {shortenAddress(addressFrom)}
+            </p>
+          </a>
 
-const TransactionCard = ({addressTo, addressFrom, timestamp, keyword, amount, url})=>{
-    return 'transaction'
-}
+          <a
+            href={`https://goerli.etherscan.io/address/${addressTo}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p className="text-white text-base ">
+              {" "}
+              Para: {shortenAddress(addressTo)}
+            </p>
+          </a>
+          <p className="text-white text-base">Quantia: {amount} ETH</p>
+          {message && (
+            <>
+              <br />
+              <p className="text-white text-base">Message: {message}</p>
+            </>
+          )}
 
-
-const Transactions = () =>{
-
-    const { currentAccount } = useContext(TransactionContext);
-
-    return (
-        <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
-            <div className='flex flex-col md:p-12 py-12 px-4 '>
-                { currentAccount ? (
-                    <h3 className='text-white text-3xl text-center my-2'>
-                            Ultimas Transações
-                    </h3>
-                ) : (
-                    <h3 className='text-white text-3xl text-center my-2'>
-                            Conect sua conta no Metamask para Visualizar suas Transações
-                    </h3>
-
-                )}
-
-                <div className='flex flex-wrap justity-center items-center mt-10'>
-                    {dummyData.reverse().map((transaction, i) =>
-                    <TransactionCard key={i} {...transaction}/>)}
-
-                </div>
-
-            </div>
-
+          <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+            <p className="text-blue-500 font-bold">{timestamp}</p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
+};
+
+const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4 ">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">
+            Ultimas Transações
+          </h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">
+            Conecte sua conta no Metamask para Visualizar suas Transações
+          </h3>
+        )}
+
+        <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
