a4e16d97866416b4960c3ead09bca401b350b425
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 14:09:40 -0300

    Ajuste componente transações

--- projeto/src/components/Transactions.jsx
+++ projeto/src/components/Transactions.jsx
@@ -9,18 +9,21 @@ const Transactions = () =>{
     const { currentAccount } = useContext(TransactionContext);
 
     return (
-        <div className='flex w-full justity-center items-center 2xl:px-20 gradient-bg-transactions'>
-            <div className='flex flex-col md:p-12 py-12 py px-4 '>
+        <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
+            <div className='flex flex-col md:p-12 py-12 px-4 '>
                 { currentAccount ? (
                     <h3 className='text-white text-3xl text-center my-2'>
-
+                            Ultimas Transações
                     </h3>
                 ) : (
                     <h3 className='text-white text-3xl text-center my-2'>
-                        Conect sua conta no Metamask para Visualizar suas Transações.
-                    </h3> 
+                            Conect sua conta no Metamask para Visualizar suas Transações
+                    </h3>
+
                 )}
+
             </div>
+
         </div>
     );
 };
