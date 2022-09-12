936ea7f7b5aed55d6db4767548489e8121497246
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 14:03:43 -0300

    Inicialização componente transações e historico e foi importado arquivo dummyData para os gifs

--- projeto/src/components/Transactions.jsx
+++ projeto/src/components/Transactions.jsx
@@ -1,6 +1,27 @@
+import React, { useContext } from 'react';
+
+import { TransactionContext } from '../context/TransactionContext';
+
+import dummyData from '../utils/dummyData';
+
 const Transactions = () =>{
+
+    const { currentAccount } = useContext(TransactionContext);
+
     return (
-        <h1>Transactions</h1>
+        <div className='flex w-full justity-center items-center 2xl:px-20 gradient-bg-transactions'>
+            <div className='flex flex-col md:p-12 py-12 py px-4 '>
+                { currentAccount ? (
+                    <h3 className='text-white text-3xl text-center my-2'>
+
+                    </h3>
+                ) : (
+                    <h3 className='text-white text-3xl text-center my-2'>
+                        Conect sua conta no Metamask para Visualizar suas Transações.
+                    </h3> 
+                )}
+            </div>
+        </div>
     );
 };
 
