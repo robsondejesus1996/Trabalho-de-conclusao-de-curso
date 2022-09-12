75825981757ca88d452d3801eb3bb9918597455d
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 16:17:09 -0300

    implementação base API Gif

---
+++ projeto/src/hooks/useFetch.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = import.meta.env.VITE_GIPHY_API
+
+const useFetch = () =>{
+    const [gifUrl, setGifUrl] = useState("");
+
+    const fetchGif = async () =>{
+        try {
+            const response = await fetch("")
+        } catch (error) {
+            
+        }
+    }
+}
