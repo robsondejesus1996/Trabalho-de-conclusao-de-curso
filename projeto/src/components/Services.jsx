a411966f470440a3dd5b8c5a12716b98468ac9e6
Robson de Jesus <robsondejesus1996@hotmail.com>
2022-09-12 13:16:59 -0300

    Inicialização componente services

--- projeto/src/components/Services.jsx
+++ projeto/src/components/Services.jsx
@@ -1,6 +1,21 @@
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeart2Fill } from 'react-icons/ri';
+
 const Services = () =>{
     return (
-        <h1>Services</h1>
+        <div className="flex w-full justify-center gradient-bg-services">
+            <div className="flex mf:flex-row flex-col items-center justity-between md:p-20 py-12 px-4">
+                <div className="flex-1 flex flex-col justity-start items-start">
+                    <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
+                        Serviços que 
+                        <br />continuamos a otimizar
+                    </h1>
+
+                </div>
+
+            </div>
+        </div>
     );
 };
 
